6792462e17cd8c7c987a730fedfec61b2e317141
mysteriouslyseeing <potatonosedfreak@gmail.com>
2023-06-01 08:52:38 +1000

    Changing package name and rename variables

--- src/main.rs
+++ src/main.rs
@@ -16,48 +16,40 @@ use lazy_static::lazy_static;
 use regex::Regex;
 
 lazy_static! {
-    static ref matches: clap::ArgMatches = {
+    static ref MATCHES: clap::ArgMatches = {
         command!()
             .arg(arg!(-r --recursive))
             .arg(Arg::new("depth").short('d').long("depth").requires("recursive").value_parser(value_parser!(usize)))
             .arg(Arg::new("show_files").short('f').long("show-files").action(ArgAction::SetTrue))
             .arg(Arg::new("show_symlinks").short('s').long("show-symlinks").action(ArgAction::SetTrue))
-            .arg(Arg::new("traverse_symlinks").short('t').long("traverse-symlinks").requires("show_symlinks").action(ArgAction::SetTrue))
             .arg(arg!(-p --path <PATH>))
             .arg(arg!(-m --matches <REGEX>))
             .get_matches()
     };
-    static ref recursive: bool = *matches.get_one::<bool>("recursive").unwrap();
-    static ref max_depth: usize = *matches.get_one::<usize>("depth").unwrap_or_else(
-        || if *recursive {&usize::MAX} else {&0}
+    static ref RECURSIVE: bool = *MATCHES.get_one::<bool>("recursive").unwrap();
+    static ref MAX_DEPTH: usize = *MATCHES.get_one::<usize>("depth").unwrap_or_else(
+        || if *RECURSIVE {&usize::MAX} else {&0}
     );
-    static ref show_files: bool = *matches.get_one::<bool>("show_files").unwrap();
-    static ref show_symlinks: bool = *matches.get_one::<bool>("show_symlinks").unwrap();
-    static ref traverse_symlinks: bool = {
-        if *matches.get_one::<bool>("traverse_symlinks").unwrap() {
-            todo!("Not yet implemented")
-        }
-        false
-    };
-    static ref root: PathBuf = PathBuf::from(matches.get_one::<String>("path").unwrap_or(
+    static ref SHOW_FILES: bool = *MATCHES.get_one::<bool>("show_files").unwrap();
+    static ref SHOW_SYMLINKS: bool = *MATCHES.get_one::<bool>("show_symlinks").unwrap();
+    static ref ROOT: PathBuf = PathBuf::from(MATCHES.get_one::<String>("path").unwrap_or(
         &current_dir().unwrap().into_os_string().into_string().unwrap())
     );
-    static ref pattern: Option<Regex> = matches.get_one::<String>("matches").map(|pat| Regex::new(&pat).expect("Regex error"));
+    static ref PATTERN: Option<Regex> = MATCHES.get_one::<String>("matches").map(|pat| Regex::new(&pat).expect("Regex error"));
 
 }
 
 fn main() {
-
-    if root.is_relative() {
+    if ROOT.is_relative() {
         let mut abs = current_dir().unwrap();
-        abs.extend(&*root);
+        abs.extend(&*ROOT);
         println!("Displaying contents of \"{}\"", abs.into_os_string().into_string().unwrap());
     } else {
-        println!("Displaying contents of \"{}\"", root.as_os_str().to_str().unwrap());
+        println!("Displaying contents of \"{}\"", ROOT.as_os_str().to_str().unwrap());
     }
 
     let mut lock = stdout().lock();
-    print_contents(&*root, 0, &mut lock);
+    print_contents(&*ROOT, 0, &mut lock);
     lock.flush().unwrap();
 }
 
@@ -67,7 +59,7 @@ fn print_contents<P: AsRef<Path>>(
     mut lock: &mut StdoutLock<'static>,
 )
 {
-    if depth > *max_depth {
+    if depth > *MAX_DEPTH {
         return;
     }
     let prefix = " ".repeat(depth);
@@ -84,9 +76,9 @@ fn print_contents<P: AsRef<Path>>(
                 if file_type.is_dir() {
                     write!(lock, "{prefix}={:?}\n", entry.file_name()).unwrap();
                     print_contents(entry.path(), depth + 1, &mut lock)
-                } else if *show_files && file_type.is_file() {
+                } else if *SHOW_FILES && file_type.is_file() {
                     write!(lock, "{prefix}-{:?}\n", entry.file_name()).unwrap();
-                } else if *show_symlinks && file_type.is_symlink() {
+                } else if *SHOW_SYMLINKS && file_type.is_symlink() {
                     write!(lock, "{prefix}&{:?}\n", entry.file_name()).unwrap();
                 }
             }
