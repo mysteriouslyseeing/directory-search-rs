61cc90eabf004dce920af7e222d832ade0167792
mysteriouslyseeing <potatonosedfreak@gmail.com>
2023-06-03 18:34:25 +1000

    Added new command, --matches

--- README.md
+++ README.md
@@ -11,14 +11,20 @@ A command to search and display directory structures.
 Usage: ds.exe [OPTIONS]
 
 Options:
-  -p, --path <PATH>    The path at which to start. If unspecified, the current directory is used.
-  -r, --recursive      Enables recursion
-  -d, --depth <DEPTH>  The depth to which to recurse. If unspecified, the depth is unlimited.
-  -f, --show-files     Displays files along with directories
-  -s, --show-symlinks  Displays symlinks along with directories
-  -h, --help           Print help
-  -V, --version        Print version
+  -p, --path <PATH>      The path at which to start. If unspecified, the current directory is used.
+  -r, --recursive        Enables recursion
+  -d, --depth <DEPTH>    The depth to which to recurse. If unspecified, the depth is unlimited.
+  -f, --show-files       Displays files along with directories
+  -s, --show-symlinks    Displays symlinks along with directories
+  -m, --matches <REGEX>  Only display directories, files and symlinks which match the given regex
+  -h, --help             Print help
+  -V, --version          Print version
 ```
+
+## Regex
+
+The regex parser is the one used by the Rust [regex](https://docs.rs/regex/1.8.3/regex/) crate. This means that the regex can match anywhere in a string. Therefore, the regex "a" matches any file name with an "a" in it. The start of a string can be matched with ^, and the end of a string can be matched with $. Most special characters, such as ".", must be escaped with a backslash ("\\")
+
 
 ## Examples
 
@@ -59,6 +65,23 @@ Displaying contents of "C:/Users/%USERNAME%/Documents"
 -"Resume.docx"
 -"Resume.pdf"
 ```
+
+### Display the entire contents of a directory recursively up to a depth of 4, only showing the files that end in .py:
+
+```console
+> ds --path "C:/Users/%USERNAME%/Documents" -r --depth 4 -f -s --matches \.py$
+Displaying contents of "C:/Users/%USERNAME%/Documents"
+="Projects"
+ ="Python"
+  ="python_project_1"
+   -"main.py"
+   -"utils.py"
+  ="python_project_2"
+   -"main.py"
+  -"python_script.py"
+```
+Note that while even though none of the folders end in .py, they are still displayed in order to show directory structure.
+
 
 ## Installation
 
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,8 @@
 use std::env::current_dir;
-use std::fs::read_dir;
-use std::io::StdoutLock;
+use std::fs::DirEntry;
 use std::io::Write;
 use std::io::stdout;
-use std::path::Path;
-use std::path::PathBuf;
 use std::println;
-
-use clap::value_parser;
-use clap::command;
-use clap::arg;
-use lazy_static::lazy_static;
-
-lazy_static! {
-    static ref MATCHES: clap::ArgMatches = {
-        command!()
-            .arg(arg!(-p --path <PATH> "The path at which to start. If unspecified, the current directory is used."))
-            .arg(arg!(-r --recursive "Enables recursion"))
-            .arg(arg!(-d --depth <DEPTH> "The depth to which to recurse. If unspecified, the depth is unlimited.").requires("recursive").value_parser(value_parser!(usize)))
-            .arg(arg!(show_files: -f --"show-files" "Displays files along with directories"))
-            .arg(arg!(show_symlinks: -s --"show-symlinks" "Displays symlinks along with directories"))
-            .get_matches()
-    };
-    static ref RECURSIVE: bool = *MATCHES.get_one::<bool>("recursive").unwrap();
-    static ref MAX_DEPTH: usize = *MATCHES.get_one::<usize>("depth").unwrap_or_else(
-        || if *RECURSIVE {&usize::MAX} else {&0}
-    );
-    static ref SHOW_FILES: bool = *MATCHES.get_one::<bool>("show_files").unwrap();
-    static ref SHOW_SYMLINKS: bool = *MATCHES.get_one::<bool>("show_symlinks").unwrap();
-    static ref ROOT: PathBuf = PathBuf::from(MATCHES.get_one::<String>("path").unwrap_or(
-        &current_dir().unwrap().into_os_string().into_string().unwrap())
-    );
-}
 
 fn main() {
     if ROOT.is_relative() {
@@ -43,8 +14,52 @@ fn main() {
     }
 
     let mut lock = stdout().lock();
-    print_contents(&*ROOT, 0, &mut lock);
+    if let Some(_) = *PATTERN {
+        for entry in fs::read_dir(&*ROOT).unwrap_or_else(generate_exit("Failed to read root directory: ")) {
+            if let Ok(entry) = entry {
+                unsafe { search_and_print(&entry, 0, &mut lock, true) };
+            }
+        }
+    } else {
+        print_contents(&*ROOT, 0, &mut lock);
+    }
     lock.flush().unwrap();
+}
+
+use std::writeln;
+use std::{path::{Path, PathBuf}, io::StdoutLock, env, fs};
+
+use clap::{arg, command};
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    static ref MATCHES: clap::ArgMatches = {
+        command!()
+            .arg(arg!(-p --path <PATH> "The path at which to start. If unspecified, the current directory is used."))
+            .arg(arg!(-r --recursive "Enables recursion"))
+            .arg(
+                arg!(-d --depth <DEPTH> "The depth to which to recurse. If unspecified, the depth is unlimited.")
+                    .requires("recursive")
+                    .value_parser(clap::value_parser!(usize))
+            )
+            .arg(arg!(show_files: -f --"show-files" "Displays files along with directories"))
+            .arg(arg!(show_symlinks: -s --"show-symlinks" "Displays symlinks along with directories"))
+            .arg(arg!(-m --matches <REGEX> "Only display directories, files and symlinks which match the given regex"))
+            .get_matches()
+    };
+    static ref RECURSIVE: bool = *MATCHES.get_one::<bool>("recursive").unwrap();
+    static ref MAX_DEPTH: usize = *MATCHES.get_one::<usize>("depth").unwrap_or_else(
+        || if *RECURSIVE {&usize::MAX} else {&0}
+    );
+    static ref SHOW_FILES: bool = *MATCHES.get_one::<bool>("show_files").unwrap();
+    static ref SHOW_SYMLINKS: bool = *MATCHES.get_one::<bool>("show_symlinks").unwrap();
+    static ref ROOT: PathBuf = PathBuf::from(MATCHES.get_one::<String>("path").unwrap_or(
+        &env::current_dir().unwrap().into_os_string().into_string().unwrap())
+    );
+    static ref PATTERN: Option<Regex> = MATCHES
+        .get_one::<String>("matches")
+        .map(|pat| Regex::new(&pat).unwrap_or_else(generate_exit("")));
 }
 
 fn print_contents<P: AsRef<Path>>(
@@ -58,7 +73,7 @@ fn print_contents<P: AsRef<Path>>(
     }
     let prefix = " ".repeat(depth);
     let dir;
-    if let Ok(dir_reader) = read_dir(&path) {
+    if let Ok(dir_reader) = fs::read_dir(&path) {
         dir = dir_reader;
     } else {
         write!(lock, "{prefix}[ACCESS DENIED]\n").unwrap();
@@ -78,4 +93,92 @@ fn print_contents<P: AsRef<Path>>(
             }
         }
     }
+}
+
+fn generate_exit<E: std::fmt::Display, O>(message: &str) -> impl FnOnce(E) -> O + '_ {
+    move |e| -> O {
+        eprintln!("{message}{e}");
+        std::process::exit(1)
+    }
+}
+
+
+/// Directory tree explorer.
+/// # SAFETY
+///
+/// Can only be called if PATTERN is Some.
+unsafe fn search_and_print(entry: &fs::DirEntry, depth: usize, lock: &mut StdoutLock, parent_printed: bool) -> Option<String> {
+    if depth > *MAX_DEPTH {
+        return None;
+    }
+    let prefix = " ".repeat(depth);
+    let file_type = entry.file_type().ok()?;
+    let pattern_matches = PATTERN
+                            .as_ref()
+                            .unwrap_unchecked()
+                            .is_match(
+                                entry
+                                    .file_name()
+                                    .as_os_str()
+                                    .to_str()
+                                    .unwrap()
+                                );
+    // Whether the entry will get displayed
+    let mut displayed = pattern_matches;
+    let mut text;
+    if file_type.is_dir() {
+        // Allocate here. Change if I want
+        text = format!("{prefix}={:?}\n", entry.file_name().as_os_str());
+        for child in fs::read_dir(entry.path()).ok()? {
+            // If the entry is valid
+            if let Ok(entry) = child {
+                // If the entry matches regex or has a child that does
+                if let Some(s) = search_and_print(&entry, depth + 1, lock, displayed && parent_printed) {
+                    match (displayed, parent_printed) {
+                        (false, true) => {
+                            write!{
+                                lock,
+                                "{text}"
+                            }.unwrap_or_else(generate_exit("Error writing to stdout: "));
+                            write!{
+                                lock,
+                                "{s}"
+                            }.unwrap_or_else(generate_exit("Error writing to stdout: "));
+                            displayed = true;
+                        },
+                        (true, true) => {
+                            write!{
+                                lock,
+                                "{s}"
+                            }.unwrap_or_else(generate_exit("Error writing to stdout: "));
+                        },
+                        (_, false) => {
+                            displayed = true;
+                            text.push_str(&s);
+                        }
+                    } 
+                    
+                }
+            }
+        }
+        if !displayed {
+            return None;
+        }
+    } else if file_type.is_file() {
+        if pattern_matches {
+            text = format!("{prefix}-{:?}\n", entry.file_name().as_os_str());
+        } else {
+            return None;
+        }
+    } else if file_type.is_symlink() {
+        if PATTERN.as_ref().unwrap_unchecked().is_match(entry.file_name().as_os_str().to_str().unwrap()) {
+            text = format!("{prefix}&{:?}\n", entry.file_name().as_os_str());  
+        } else {
+            return None
+        }
+    } else {
+        unreachable!()
+    }
+
+    Some(text)
 }
